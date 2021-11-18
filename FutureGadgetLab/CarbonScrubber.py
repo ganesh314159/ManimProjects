880758fe483a9b40555a35dee1b11bc42aa6132a
Ganesh314159 <ganesh314159@gmail.com>
2021-11-18 21:02:48 +0530

    Created graph for aAFC

--- FutureGadgetLab/CarbonScrubber.py
+++ FutureGadgetLab/CarbonScrubber.py
@@ -39,3 +39,47 @@ class AFC(Scene):
         are_label=Tex("ARE%").next_to(graph_are, UP, buff=0.1)
 
         self.add(axes, axes_labels, graph_KF, kf_label, graph_R2, r2_label, graph_are, are_label, graph_n, n_label,)
+
+class aAFC(Scene):
+    def construct(self):
+        axes = Axes(
+            x_range=[0, 60, 10],
+            y_range=[0, 2.5, 0.5],
+            x_length=9,
+            y_length=6,
+            axis_config={"color": WHITE},
+            x_axis_config={
+                "numbers_to_include": np.arange(0, 60, 10),
+                # "numbers_with_elongated_ticks": np.arange(0, 60, 10),
+            },
+            y_axis_config={
+                "numbers_to_include": np.arange(0, 2.5, 0.5),
+            },
+            tips=False,
+        )
+        axes_labels = axes.get_axis_labels(
+            x_label=Tex("Temperature").scale(0.75),
+            y_label=Tex("AFC").scale(0.75)
+        )
+        x_vals = [25, 40, 55]
+        y_vals_KF = [0.164, 0.102, 0.063]
+        y_vals_n = [1.64, 1.54, 1.50]
+        y_vals_R2 = [0.9998, 0.9998, 0.9993]
+        y_vals_are = [1.89, 1.76, 1.54]
+        # y_vals_
+
+        graph_KF=axes.plot_line_graph(x_values=x_vals, y_values=y_vals_KF, line_color=PURE_RED)
+        kf_label=Tex("KF").next_to(graph_KF, UP, buff=0.1)
+        graph_n=axes.plot_line_graph(x_values=x_vals, y_values=y_vals_n, line_color=GREEN)
+        n_label=Tex("n").next_to(graph_n, DOWN, buff=0.1)
+        graph_R2=axes.plot_line_graph(x_values=x_vals, y_values=y_vals_R2, line_color=BLUE)
+        r2_label=Tex("$R^2$").next_to(graph_R2, DOWN, buff=0.1)
+        graph_are=axes.plot_line_graph(x_values=x_vals, y_values=y_vals_are, line_color=YELLOW)
+        are_label=Tex("ARE%").next_to(graph_are, UP, buff=0.1)
+
+        self.add(axes, axes_labels, graph_KF, kf_label, graph_R2, r2_label, graph_are, are_label, graph_n, n_label,)
+
+
+
+
+
